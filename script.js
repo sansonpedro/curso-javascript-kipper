997eba181cdcc62e44488ff08ac26785286f788e
Pedro Henrique Sanson <156597965+sansonpedro@users.noreply.github.com>
2025-10-09 20:15:01 -0300

    entendendo e fechando funções

--- script.js
+++ script.js
@@ -23,13 +23,38 @@
 // Incremento: i++
 
 // Para cada coisa... eu faço tal coisa
-for (let i =0; i < 5; i++) {
-  console.log(`O valor do índice é ${i}`)
-}
+// for (let i =0; i < 5; i++) {
+//   console.log(`O valor do índice é ${i}`);
+// }
 
 // Enquanto tal coisa... eu faço tal coisa
 // let i = 0;
 // while (1 < 5){
-//   console.log("Edxecutandio loop");
 //   i++;
+//   console.log("Executando loop", i);
 // }
+
+// VOID => quando a função não retorna nada
+// RETURN => qunado a função retorna algo
+
+let idades = [18, 25, 32, 11, 9, 45];
+
+let somaDasIdades = 0;
+for (let i = 0; i < idades.length; i ++){
+  let idade = idades[i];
+  somaDasIdades = somarIdades(somaDasIdades, idade);
+}
+
+console.log("Soma das idades: ", somaDasIdades)
+
+function verificaSeEMaiorDeIdade(idade) {
+  return idade >= 18;
+}
+
+function somarIdades(somatorio, idade){
+  return somatorio + idade;
+}
+
+// VAR -> respeita escobo global e de função
+// LET e CONST -. respeitam escopo global, de função e de bloco
+// bloco pode ser qualquer coisa que use chaves { ... }
